6cc6fad972313f2896fd3b01d287a48deb46ce03
SeymaArslan <seymarslannn@gmail.com>
2023-09-23 15:24:13 +0300

    Add logoView details

--- tip-calculator/view/LogoView.swift
+++ tip-calculator/view/LogoView.swift
@@ -8,6 +8,52 @@
 import UIKit
 
 class LogoView: UIView {
+    
+    //MARK: - UI View Objects
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "icCalculatorBW"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "S.A TIP",
+            attributes: [.font: ThemeFont.demibold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 4))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: "Hesaplama",
+            font: ThemeFont.demibold(ofSize: 20),
+            textAlignment: .left)
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        return view
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            imageView,
+            vStackView
+        ])
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+
     init() {
         super.init(frame: .zero)  // Otomatik düzeni kaldırmak için zero kullandık
         layout()
@@ -18,7 +64,16 @@ class LogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .red
+        addSubview(hStackView)
+        
+        hStackView.snp.makeConstraints { make in
+            // equalToSuperview ile logo alanı UIView'ın üstüne ve altına sabitlenecektir.
+            make.top.bottom.equalToSuperview()
+            make.center.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
     }
-    
 }
