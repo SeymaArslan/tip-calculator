bfb4558a4fee177062fe0779d5542de4a37beae9
SeymaArslan <seymarslannn@gmail.com>
2023-09-26 14:59:35 +0300

    Setup ViewModel start and add ViewModel struct

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #  <#Title#>
 
+VM uygulamanın beyni hesaplamalar burada olacak yani işin mantığı burada.. Kullanıcının View ile olan etkileşimi de diyebiliriz. Hem kullanıcı dokunduğunda hem de işlenmesi için model
---
+++ tip-calculator/model/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Seyma on 26.09.2023.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
