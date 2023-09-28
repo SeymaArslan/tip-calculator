2b48cfac4f7caf66f70ce78179c5e9741adddf07
SeymaArslan <seymarslannn@gmail.com>
2023-09-28 16:36:38 +0300

    test logo view double tap and add more tests

--- tip-calculatorTests/tip_calculatorTests.swift
+++ tip-calculatorTests/tip_calculatorTests.swift
@@ -18,8 +18,13 @@ final class tip_calculatorTests: XCTestCase {
     
     private let logoViewTapSubject = PassthroughSubject<Void, Never>()
     
+    private var audioPlayerService: MockAudioPlayerService!
+    
     override func setUp() {  // class olan değil bu setup.. Bir test her çağrıldığında setUp metodu çağırılır ve sut un bir örneğini oluşturur
-        sut = .init() // init audioPlayerService içeriyor. Bu oluşturduğumuz protocol ve hiçbir şey aktarmamıza gerek yok çünkü şu an ses oynatıcısını test etmiyoruz
+//        sut = .init() // init audioPlayerService içeriyor. Bu oluşturduğumuz protocol ve hiçbir şey aktarmamıza gerek yok çünkü şu an ses oynatıcısını test etmiyoruz
+        audioPlayerService = .init()
+        sut = .init(audioPlayerService: audioPlayerService)
+        
         cancellables = .init()
         super.setUp()
     }
@@ -45,6 +50,61 @@ final class tip_calculatorTests: XCTestCase {
         }.store(in: &cancellables)
     }
     
+    func testResultWithoutTipFor2Person() {  // test butonu gelmezse final class alanından itibaren test ediyoruz
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith10PercentTipFor2Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipFor4Person() {
+        let bill: Double = 200.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
+        }.store(in: &cancellables)
+    }
+    
+    func testSoundPlayedAndCalculatorResetOnLogoViewTap() {
+        // GİVEN
+        let input = buildInput(bill: 100, tip: .tenPercent, split: 2)
+        let output = sut.transform(input: input)
+        let expectation1 = XCTestExpectation(description: "reset calculator called")
+        let expectation2 = audioPlayerService.expectation
+        //THEN
+        output.resetCalculatorPublisher.sink { _ in
+            expectation1.fulfill()
+        }.store(in: &cancellables)
+        
+        //WHEN
+        logoViewTapSubject.send()
+        wait(for: [expectation1, expectation2], timeout: 1.0)
+    }
+    
     private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
         return .init(
             billPublisher: Just(bill).eraseToAnyPublisher(),
@@ -53,4 +113,11 @@ final class tip_calculatorTests: XCTestCase {
             logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher())
     }
     
+}
+
+class MockAudioPlayerService: AudioPlayerService {
+    var expectation = XCTestExpectation(description: "playSound is called")
+    func playSound() {
+        expectation.fulfill()
+    }
 }
